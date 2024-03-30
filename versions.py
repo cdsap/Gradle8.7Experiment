ae91000c2d4dd5a30757b08c4249c7db522f6799
Inaki Villar <inaki.seri@gmail.com>
2024-03-30 07:46:38 -0700

    let see what happens

---
+++ versions.py
@@ -0,0 +1,22 @@
+import json
+
+# Initial variables and their values
+workers = [2]
+gc_gradle = ["-XX:+UseG1GC", "-XX:+UseParallelGC"]
+heap_gradle = ["-Xmx4g", "-Xmx6g"]
+repetitions = 2  # Number of times to repeat each combination
+
+combinations = []
+for worker in workers:
+    for gc in gc_gradle:
+        for heap in heap_gradle:
+            for rep in range(repetitions):
+                combinations.append({
+                    "WORKERS": str(worker),
+                    "GC_GRADLE": gc,
+                    "HEAP_GRADLE": heap,
+                    "REPETITION": rep + 1  # Repetition number (1-30)
+                })
+
+# Print the JSON string to be used in the workflow
+print(json.dumps({"include": combinations}))
