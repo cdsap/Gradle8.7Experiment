eac08e2feca2fa4cd7af80fc882de75c31a04c07
Inaki Villar <inaki.seri@gmail.com>
2024-03-30 09:54:54 -0700

    180 repetitions

--- versions.py
+++ versions.py
@@ -1,13 +1,9 @@
-import json
-
-# Initial variables and their values
 workers = [2]
 gc_gradle = ["-XX:+UseG1GC", "-XX:+UseParallelGC"]
 heap_gradle = ["-Xmx4g", "-Xmx6g", "-Xmx8g"]
 heap_kotlin = ["-Xmx4g", "-Xmx6g", "-Xmx8g"]
 kc_gradle = ["-XX:+UseG1GC", "-XX:+UseParallelGC"]
-repetitions = 5
-
+repetitions = 5  # Ensure this applies to all combinations
 
 combinations = []
 for worker in workers:
@@ -15,27 +11,15 @@ for worker in workers:
         for kc_g in kc_gradle:
             for heap_g in heap_gradle:
                 for heap_k in heap_kotlin:
-                    # Only adding combinations where heap sizes are different to avoid repetition
-                    if heap_g == heap_k:
-                        for rep in range(repetitions):
-                            combinations.append({
-                                "WORKERS": str(worker),
-                                "GC_GRADLE": gc_g,
-                                "HEAP_GRADLE": heap_g,
-                                "KC_GRADLE": kc_g,
-                                "HEAP_KOTLIN": heap_k,
-                                "REPETITION": rep + 1  # Adjusted number of repetitions
-                            })
-                    else:
+                    for rep in range(repetitions):  # Apply repetitions to all
                         combinations.append({
                             "WORKERS": str(worker),
                             "GC_GRADLE": gc_g,
                             "HEAP_GRADLE": heap_g,
                             "KC_GRADLE": kc_g,
                             "HEAP_KOTLIN": heap_k,
-                            "REPETITION": 1  # Only one repetition for different heap sizes
+                            "REPETITION": rep + 1
                         })
-
 
 # Print the JSON string to be used in the workflow
 print(json.dumps({"include": combinations}))
